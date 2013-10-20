fde53ece7a39fd21f2f72e29b414821079657744
Ben Getsug <ben@shift31.com>
2013-10-20 01:30:57 -0500

    initial commit

---
+++ hostbase-importer-puppetdb.php
@@ -0,0 +1,62 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Shift31\HostbaseClient;
+use Httpful\Request;
+use Httpful\Response;
+
+$config = parse_ini_file(__DIR__ . '/config.ini');
+$puppetDbBaseUrl = $config['puppetDbBaseUrl'];
+
+$HbClient = new HostbaseClient($config['hostbaseUrl']);
+
+// get nodes
+$response = Request::get("$puppetDbBaseUrl/v2/nodes")
+	->sendsAndExpects('application/json')
+	->send();
+
+if ($response instanceof Response) {
+	$nodes = $response->body;
+
+	foreach ($nodes as $node) {
+
+		$data = array();
+		$fqdn = $node->name;
+		$data['fqdn'] = $fqdn;
+
+		// get facts
+		$response = Request::get("$puppetDbBaseUrl/v2/nodes/$node->name/facts")
+			->sendsAndExpects('application/json')
+			->send();
+
+		if ($response instanceof Response) {
+			$facts = $response->body;
+
+			foreach ($facts as $fact) {
+				if (preg_match('/ssh|swap|_lo|last_run|memoryfree|path|swapfree|uptime|uniqueid|clientcert/', $fact->name)) continue;
+				if ($fact->name == 'ps') continue;
+				$data[$fact->name] = $fact->value;
+			}
+		}
+
+		echo "Importing $fqdn...";
+
+		try {
+			// add
+			echo "adding...\n";
+			$HbClient->store($data);
+		} catch (Exception $e) {
+
+			echo $e->getMessage() . PHP_EOL;
+
+			try {
+				// update
+				echo "updating...\n";
+				$HbClient->update($fqdn, $data);
+			} catch (Exception $e) {
+				echo $e->getMessage() . PHP_EOL;
+			}
+		}
+	}
+}
